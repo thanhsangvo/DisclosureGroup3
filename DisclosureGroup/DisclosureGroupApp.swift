c381db5c23db731c58b5d18a5d6710982d9a68cc
thanhsangvo <pl.thanhsang0710@gmail.com>
2020-07-02 16:54:55 +0700

    Initial Commit

---
+++ DisclosureGroup/DisclosureGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisclosureGroupApp.swift
+//  DisclosureGroup
+//
+//  Created by Võ Thanh Sang on 7/2/20.
+//
+
+import SwiftUI
+
+@main
+struct DisclosureGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
